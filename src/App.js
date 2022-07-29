762e35938f6cb52e3e8cfeec8fae61f972c3fb23
Thanyapisit Buaprakong <nyman3527@gmail.com>
2022-07-29 23:58:30 +0700

    create newversion kepsci template.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Navbar from './components/Navbar'; 
+import ImageSlider from './components/ImageSlider'; 
+
+function App() {
+    return (
+    < Navbar />
+    )
+}
+
+export default App; 
