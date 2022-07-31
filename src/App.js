b1afbf0311e42a9e85fc7fdba578001c6d20d223
Thanyapisit Buaprakong <nyman3527@gmail.com>
2022-08-01 01:22:12 +0700

    Complete slider data :zap:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar'; 
 import ImageSlider from './components/ImageSlider'; 
+import { SliderData } from './components/SliderData';
+import './App.css'
 
 function App() {
     return (
     <>
     < Navbar />
-    < ImageSlider/> 
+    < ImageSlider slides={SliderData}/> 
     </>
     )
 }
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -2,18 +2,35 @@ import React, { useState } from 'react'
 import { SliderData } from './SliderData'
 import {FaArrowAltCircleLeft, FaArrowAltCircleRight} from 'react-icons/fa'
 
-const ImageSlider = () => {
-const [current, Setcurrent] = useState(0)
-const length = SliderData.length
+const ImageSlider = ({slides}) => {
+    const [current, setCurrent] = useState(0)
+    const length = slides.length
+
+    const nextSlide = () => { 
+        setCurrent(current === length - 1 ? 0 : current + 1) 
+    }
+
+    const prevSlide = () => { 
+        setCurrent(current === 0 ? length - 1 : current - 1) 
+    } 
+    if (!Array.isArray(slides) || slides.length <= 0){
+        return null
+    }
 
     return (
-        <setion className="slider">
-            <FaArrowAltCircleLeft className="left-arrow"/>
-            <FaArrowAltCircleRight className="right-arrow"/>
+        <section className="slider">
+            <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide}/>
+            <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
             {SliderData.map((slide, index) => {
-                return <img src={slide.image} alt='travel image' />
+                return (
+                <div className={index === current ? 'slide active': 'slide'} key={index}> 
+                {index === current &&(
+                    <img src={slide.image} alt='travel image' className='image'/>
+            )}   
+                </div>
+                )
             })}
-        </setion>
+        </section>
     )
 }
 
